f7074235e0e179e1baebcdf98262aa5ce3e768d7
Josef Pihrt <josef@pihrt.net>
2023-07-20 13:56:47 +0200

    update

--- src/DotMarkdown/Linq/MFencedCodeBlock.cs
+++ src/DotMarkdown/Linq/MFencedCodeBlock.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.Diagnostics;
 using System;
+using System.Diagnostics;
 
 namespace DotMarkdown.Linq;
 
