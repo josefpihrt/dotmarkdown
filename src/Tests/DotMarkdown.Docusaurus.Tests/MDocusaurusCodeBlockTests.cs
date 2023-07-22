6a501436bb58535066bd034ba8da58249ef5562e
Josef Pihrt <josef@pihrt.net>
2023-07-22 16:23:13 +0200

    update

--- src/Tests/DotMarkdown.Docusaurus.Tests/MDocusaurusCodeBlockTests.cs
+++ src/Tests/DotMarkdown.Docusaurus.Tests/MDocusaurusCodeBlockTests.cs
@@ -1,9 +1,9 @@
 ﻿// Copyright (c) Josef Pihrt. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using DotMarkdown.Docusaurus.Linq;
 using Xunit;
 using static DotMarkdown.Docusaurus.Tests.DocusaurusTestHelpers;
 using static DotMarkdown.Tests.TestHelpers;
-using DotMarkdown.Docusaurus.Linq;
 
 namespace DotMarkdown.Docusaurus.Tests;
 
