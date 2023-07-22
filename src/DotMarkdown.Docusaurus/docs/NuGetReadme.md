7d3ea97425ae51556fefb1b341125cc6de5b21fd
Josef Pihrt <josef@pihrt.net>
2023-07-22 16:43:15 +0200

    update

--- src/DotMarkdown.Docusaurus/docs/NuGetReadme.md
+++ src/DotMarkdown.Docusaurus/docs/NuGetReadme.md
@@ -6,3 +6,11 @@ Extension of DotMarkdown package for Docusaurus markdown.
 
 * .NET Standard 1.3
 * .NET Framework 4.6
+
+## Usage
+
+### Commonly Used Types
+
+* `DotMarkdown.Docusaurus.DocusaurusMarkdownWriter`
+* `DotMarkdown.Docusaurus.DocusaurusMarkdownFormat`
+* `DotMarkdown.Docusaurus.Linq.DocusaurusMarkdownFactory`
--- tools/generate_ref_docs.ps1
+++ tools/generate_ref_docs.ps1
@@ -5,7 +5,7 @@ dotnet build "../../Roslynator/src/CommandLine.sln" --no-restore /p:Configuratio
 
 & $roslynatorExe generate-doc "../src/DotMarkdown.sln" `
  --properties "Configuration=Release" `
- --projects "DotMarkdown(netstandard1.3)" `
+ --projects "DotMarkdown(netstandard1.3)" "DotMarkdown.Docusaurus(netstandard1.3)" `
  --heading "DotMarkdown Reference" `
  -o "build/ref" `
  --host docusaurus `
@@ -16,7 +16,7 @@ dotnet build "../../Roslynator/src/CommandLine.sln" --no-restore /p:Configuratio
 
  & $roslynatorExe generate-doc-root "../src/DotMarkdown.sln" `
   --properties "Configuration=Release" `
-  --projects "DotMarkdown(netstandard1.3)" `
+  --projects "DotMarkdown(netstandard1.3)" "DotMarkdown.Docusaurus(netstandard1.3)" `
   -o "build/ref.md" `
   --host docusaurus `
   --heading "DotMarkdown Reference" `
